cac3f149d42167aa7a4bf05bfb0755ed59b7f5a5
kumancev <kumancev1@gmail.com>
2022-05-12 16:28:38 +0300

    Add new algorithm

---
+++ selection_sort.js
@@ -0,0 +1,24 @@
+function selectionSort(Arr) { 
+    let n = Arr.length;
+        
+    for(let i = 0; i < n; i++) {
+        // Находим наименьшее число в правой части массива
+        let min = i;
+        for(let j = i; j < n; j++) {
+            if(Arr[j] < Arr[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             // Заменяем элементы
+             let tmp = Arr[i]; 
+             Arr[i] = Arr[min];
+             Arr[min] = tmp;      
+        }
+    }
+    return Arr;
+}
+
+let inputArr = [5, 2, 4, 6, 1, 3];
+selectionSort(inputArr);
+console.log(inputArr);
